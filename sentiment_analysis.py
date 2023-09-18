3ce122369ed5401f4cd03bff52961959deef24d6
satvikel4 <56204066+satvikel4@users.noreply.github.com>
2023-09-17 22:11:51 -0700

    sentiment analysis functionality

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,3 +1,28 @@
 from textblob import TextBlob
 from dataclasses import dataclass
 
+@dataclass
+class Mood:
+    classification: str
+    sentiment: float
+
+def get_sentiment(text: str, *, threshold: float) -> Mood:
+    sentiment = TextBlob(text).sentiment.polarity
+
+    positive_threshold = threshold
+    negative_threshold = -threshold
+
+    if sentiment >= positive_threshold:
+        return Mood("Positive", sentiment)
+    elif sentiment <= negative_threshold:
+        return Mood("Negative", sentiment)
+    else:
+        return Mood("Neutral", sentiment)
+
+if __name__ == '__main__':
+    print("Enter 'submit' to exit sentiment analysis")
+    text = ""
+    while text != "submit":
+        text = input("Enter text for sentiment analysis: ")
+        mood = get_sentiment(text, threshold = 0.33)
+        print("Classification: " + mood.classification + ", Sentiment: " + mood.sentiment)
