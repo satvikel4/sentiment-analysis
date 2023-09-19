bf7cdecf156faebb74527616a78012bf41842787
satvikel4 <56204066+satvikel4@users.noreply.github.com>
2023-09-18 22:51:36 -0700

    made code cleaner

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,24 +6,26 @@ class Mood:
     classification: str
     sentiment: float
 
-def get_sentiment(text: str, *, threshold: float) -> Mood:
+def get_sentiment(text: str, threshold: float) -> Mood:
     sentiment = TextBlob(text).sentiment.polarity
 
-    positive_threshold = threshold
-    negative_threshold = -threshold
-
-    if sentiment >= positive_threshold:
-        return Mood("Positive", sentiment)
-    elif sentiment <= negative_threshold:
-        return Mood("Negative", sentiment)
+    if sentiment >= threshold:
+        classification = "Positive"
+    elif sentiment <= -threshold:
+        classification = "Negative"
     else:
-        return Mood("Neutral", sentiment)
+        classification = "Neutral"
+
+    return Mood(classification, sentiment)
 
 if __name__ == '__main__':
     print("Enter 'quit' to exit sentiment analysis")
-    text = input("Enter text for sentiment analysis: ")
-    while text.lower() != "quit":
-        mood = get_sentiment(text, threshold=0.33)
-        print("Classification: " + mood.classification + ", Sentiment: " + str(mood.sentiment))
+    
+    while True:
         text = input("Enter text for sentiment analysis: ")
-
+        
+        if text.lower() == "quit":
+            break
+            
+        mood = get_sentiment(text, threshold=0.33)
+        print(f"Classification: {mood.classification}, Sentiment: {mood.sentiment}")
